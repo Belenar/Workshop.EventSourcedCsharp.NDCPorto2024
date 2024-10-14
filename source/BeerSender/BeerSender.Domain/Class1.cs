46d194eee214d39bb2925ce2f7552512b95f5d13
nylto <tore.nylund.hagensen@helsedir.no>
2024-10-14 13:21:31 +0100

    1

--- source/BeerSender/BeerSender.Domain/Class1.cs
+++ source/BeerSender/BeerSender.Domain/Class1.cs
@@ -1,7 +1,28 @@
 ﻿namespace BeerSender.Domain
 {
-    public class Class1
+    public class BeerBottle
     {
+        public string Name { get; set; }
+        public double Percentage { get; set; }
+        public BeerType BeerType { get; set; }
+        public string Brewery { get; set; }
+    }
 
+    public record AddBeerBottle
+    (
+        Guid BoxId,
+        BeerBottle BeerBottle
+    );
+    
+    public record BeerBottleAdded
+    (
+        BeerBottle BeerBottle
+    );
+
+    public enum BeerType
+    {
+        Ipa,
+        Stout,
+        Sour
     }
 }
