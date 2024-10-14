5d9cfb74340cbd03248469b67fd24bc24116606c
Endre Skogen <esk@progit.no>
2024-10-14 14:41:37 +0200

    beerbottle events

--- source/BeerSender/BeerSender.Domain/Class1.cs
+++ source/BeerSender/BeerSender.Domain/Class1.cs
@@ -3,9 +3,16 @@
     public class BeerBottle
     {
         public string Name { get; set; }
-        public double Percentage { get; set; }
+        public double AlcoholPercentage { get; set; }
         public BeerType BeerType { get; set; }
         public string Brewery { get; set; }
+    }
+    
+    public enum BeerType
+    {
+        Ipa,
+        Stout,
+        Sour
     }
 
     public record AddBeerBottle
@@ -19,10 +26,31 @@
         BeerBottle BeerBottle
     );
 
-    public enum BeerType
+    public record FailedToAddBeerBottle(FailedToAddBeerBottle.FailedReasonType FailedReason)
     {
-        Ipa,
-        Stout,
-        Sour
+        public enum FailedReasonType
+        {
+            BoxWasFull
+        }
     }
+
+
+    public class Box : Aggregate
+    {
+        public List<BeerBottle> BeerBottles { get; } = new();
+
+        public void Apply(BeerBottleAdded @event)
+        {
+            this.BeerBottles.Add(@event.BeerBottle);
+        }
+    }
+
+    public record AddBox(int DesiredCapacity);
+
+    public abstract class Aggregate
+    {
+        public void Apply(object @event) {}
+    }
+    
+
 }
