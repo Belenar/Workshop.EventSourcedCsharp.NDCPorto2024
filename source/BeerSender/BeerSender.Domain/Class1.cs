9588e7627c7bf746cbedf8b688e028e72ac4c9ab
Stef Thijssen <Stef.Thijssen@infosupport.com>
2024-10-14 14:57:05 +0200

    .

--- source/BeerSender/BeerSender.Domain/Class1.cs
+++ source/BeerSender/BeerSender.Domain/Class1.cs
@@ -38,14 +38,36 @@
     public class Box : Aggregate
     {
         public List<BeerBottle> BeerBottles { get; } = new();
+        public BoxCapacity BoxCapacity { get; set; }
 
         public void Apply(BeerBottleAdded @event)
         {
             this.BeerBottles.Add(@event.BeerBottle);
         }
+
+        public void Apply(BoxAdded @event)
+        {
+            BoxCapacity = @event.boxCapacity;
+        }
     }
 
     public record AddBox(int DesiredCapacity);
+
+    public record BoxAdded(BoxCapacity boxCapacity);
+
+    public record BoxCapacity
+        (int NumberOfSpots)
+    {
+        public static BoxCapacity Create(int desiredCapacity) 
+            => desiredCapacity switch
+                {
+                    <= 6 => new BoxCapacity(6),
+                    <= 12 => new BoxCapacity(12),
+                    <= 24 => new BoxCapacity(24),
+                    _ => throw new InvalidOperationException("Box capacity can't be bigger than 24")
+                };
+
+    }
 
     public abstract class Aggregate
     {
